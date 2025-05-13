b580a021d4c417844d44f70f57720f69ddfa1453
Mohamed-sa <mohamedibrahim.sa999@gmail.com>
2025-05-14 01:10:59 +0300

    ..
    ..

--- README.md
+++
@@ -1,4 +0,0 @@
-# myproject
-This is test repo
-This repo is created by mohamed
-<h1>Hello</h1>
--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
 /**
- * Index file
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * @package Astra
- * @since Astra 1.0.0
+ * @package WordPress
  */
 
-/* Silence is golden, and we agree. */
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
